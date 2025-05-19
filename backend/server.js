87b65496db3612e044092edda7df6f67c5055e14
Satyam <satyamworks20@gmail.com>
2025-05-19 13:53:59 +0530

    few changes added

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,6 @@ app.use(cors());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pragrati_mitar', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => {
